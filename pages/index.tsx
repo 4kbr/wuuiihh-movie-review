cc6234b5ab3117618f08a70b05a17e80eaec8ce5
akbar-human <akbar@hub9.biz>
2023-01-18 17:05:00 +0700

    add components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,30 @@
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import Results from "@/components/Results";
+import Head from "next/head";
 import React from "react";
 
 export default function index() {
-  return <div>index</div>;
+  return (
+    <div>
+      <Head>
+        <title>Wuiih - Movie Review</title>
+        <meta
+          name="description"
+          content="Wuiih adalah platform untuk melihat review moview"
+        />
+      </Head>
+      {/* Header */}
+      <Header />
+      {/* Header - Close */}
+
+      {/* Navbar */}
+      <Navbar />
+      {/* Navbar - Close*/}
+
+      {/* Results */}
+      <Results />
+      {/* Results - Close*/}
+    </div>
+  );
 }
