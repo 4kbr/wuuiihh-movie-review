ef0eaf4edb7484214aea78ae50eea172ec5ef80f
akbar-human <akbar@hub9.biz>
2023-01-19 11:23:19 +0700

    add navbar component and create request.ts for use for fetching

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { useRouter } from "next/router";
+import request from "../utils/requests";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-center bg-gray-500 text-gray-200 select-none text-xl lg:text-2xl">
+      {Object.entries(request).map(([key, { title, url }]: any, i: number) => (
+        <h2
+          onClick={() => router.push(`?genre=${key}`)}
+          className="m-6 cursor-pointer hover:text-white active:text-blue-400"
+          key={i}
+        >
+          {title}
+        </h2>
+      ))}
+    </div>
+  );
 }
---
+++ utils/requests.ts
@@ -0,0 +1,13 @@
+const API_KEY: string = process.env.API_KEY!;
+const fetchApi = {
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+};
+
+export default fetchApi;
